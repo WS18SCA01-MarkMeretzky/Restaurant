1f86c3f53e5c7dd20d4fdaf43631c1b5473cab92
Mark Meretzky <mark.meretzky@gmail.com>
2019-03-23 07:39:44 -0400

    Update AppDelegate.swift

--- Restaurant/AppDelegate.swift
+++ Restaurant/AppDelegate.swift
@@ -17,13 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        NotificationCenter.default.addObserver(self, selector: #selector(updateOrderBadge), name: MenuController.orderUpdatedNotification, object: nil);
-        
-        orderTabBarItem = (self.window!.rootViewController! as! UITabBarController).viewControllers![1].tabBarItem;
-        
         let temporaryDirectory: String =  FileManager.default.temporaryDirectory.path;   //p. 945
         let urlCache: URLCache = URLCache(memoryCapacity: 25_000_000, diskCapacity: 50_000_000, diskPath: temporaryDirectory);
         URLCache.shared = urlCache;
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateOrderBadge), name: MenuController.orderUpdatedNotification, object: nil);
+        
+        orderTabBarItem = (self.window!.rootViewController! as! UITabBarController).viewControllers![1].tabBarItem;
         return true;
     }
     
